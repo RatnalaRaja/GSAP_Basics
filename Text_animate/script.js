8c32f6a993537c9713cc7d2079d0a7f81b11b56a
Ratnala Raja <21mh1a4949@acoe.edu.in>
2024-06-07 10:22:43 +0530

    Text Animation

---
+++ Text_animate/script.js
@@ -0,0 +1,103 @@
+function loco(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+
+
+
+//for One element
+
+// var h1 =document.querySelector("#firsth1").textContent;
+
+// var splitter =h1.split("")
+
+// var clut=" ";
+// splitter.forEach(function(h){
+
+//     clut+=`<span>${h}</span>`
+// })
+
+// document.querySelector("#firsth1").innerHTML=clut
+
+
+
+function textsplitter(){
+    var allelements=document.querySelectorAll("#page2 h1");
+
+allelements.forEach(function(allelement){
+
+    var h1text=allelement.textContent;
+    var splitter=h1text.split("");
+    var clut=""
+    splitter.forEach(function(e){
+        clut+=`<span>${e}</span>`
+
+    })
+    allelement.innerHTML=clut;
+
+})
+}
+
+function gsapAni(){
+    gsap.to("#page2 h1 span",{
+        color:"#e3e3c4",
+        stagger:0.1,
+        scrollTrigger:{
+            trigger:"#page2 h1",
+            scroller:"#main",
+            start:"top 50%",
+            end:"top -10%",
+            scrub:2,
+        }
+    })
+}
+loco();
+textsplitter();
+
+gsapAni();
+
+
+
+
+//=======================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
